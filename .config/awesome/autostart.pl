15c84d3375e2a0afff814b8e1da65a1791c37e47
Hacksign <evilsign@gmail.com>
2014-11-07 03:02:15 +0800

    adjust some command order

--- .config/awesome/autostart.pl
+++ .config/awesome/autostart.pl
@@ -3,11 +3,11 @@
 my %programmes = (
 	'volumeicon' => 'volumeicon &',
 	'xcompmgr' => 'xcompmgr -cCfF -r7 -o.65 -l-10 -t-8 -D1 &',
-	'wicd-gtk' => 'wicd-gtk -t&',
+	'wicd-client' => 'wicd-gtk -t&',
 	'fcitx' => 'fcitx -d &',
 	'launchy' => 'launchy &',
+	'autossh' => 'autossh -M 20000 -D 9050 -CnN antigfw\@vps &',
 	'EvernoteTray' => 'wine  "/home/hacksign/.wine/drive_c/Program Files/Evernote/Evernote/EvernoteTray.exe"&',
-	'autossh' => 'autossh -M 20000 -D 9050 -CnN antigfw\@www.hacksign.cn -f&'
 );
 foreach my $key(keys %programmes){
 	system($programmes{$key}) if `pgrep $key|wc -l` == 0;
