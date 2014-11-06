fda9fa09560c1bc0cf2f4855838521f26733b6fc
Hacksign <evilsign@gmail.com>
2014-11-07 00:15:33 +0800

    add awesome theme 'arch', modify parameters of xcompmgr in awesome rc.lua file

--- .config/awesome/autostart.pl
+++ .config/awesome/autostart.pl
@@ -2,13 +2,11 @@
 
 my %programmes = (
 	'volumeicon' => 'volumeicon &',
-	'xcompmgr' => 'xcompmgr -s -n -Cc -fF -l-10 -O-10 -D1 -t-3 -l-4 -r4&',
-	'wicd-client' => 'wicd-gtk -t&',
+	'xcompmgr' => 'xcompmgr -cCfF -r7 -o.65 -l-10 -t-8 -D1 &',
+	'wicd-gtk' => 'wicd-gtk -t&',
 	'fcitx' => 'fcitx -d &',
 	'launchy' => 'launchy &',
 	'EvernoteTray' => 'wine  "/home/hacksign/.wine/drive_c/Program Files/Evernote/Evernote/EvernoteTray.exe"&',
-	'FSCapture' => 'wine "/home/hacksign/Software/CaptureScreen/FSCapture.exe -Silent"&',
-	'udiskie' => 'udiskie&',
 	'autossh' => 'autossh -M 20000 -D 9050 -CnN antigfw\@www.hacksign.cn -f&'
 );
 foreach my $key(keys %programmes){
