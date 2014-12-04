058ac34ad08e0d35a80a02a35f9612af4463a566
Hacksign <evilsign@gmail.com>
2014-12-04 13:49:00 +0800

    add MyEnTunnel to my autostart to provide tray icon notice of ssh tunnel

--- .config/awesome/autostart.pl
+++ .config/awesome/autostart.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/perl
 
 my %programmes = (
+	'xfdesktop' => 'xfdesktop &',
 	'volumeicon' => 'volumeicon &',
 	'thunar' => 'thunar --daemon &',
 	'compton'	=>	'compton -bf -o 0.85 -m 0.85 -D9 -O 0.05 -I 0.05 &',
 	'wicd-client' => 'wicd-client -t&',
 	'fcitx' => 'fcitx -d &',
 	'launchy' => 'launchy &',
-	'autossh' => 'autossh -M 20000 -D 9050 -CnN antigfw\@vps &',
 	'goldendict' => 'goldendict &',
 	'thunderbird' => 'thunderbird &',
+	'myentunnel.exe' => 'wine "/home/hacksign/.wine/drive_c/Program Files/MyEnTunnel/myentunnel.exe" &',
 	'EvernoteTray' => 'wine  "/home/hacksign/.wine/drive_c/Program Files/Evernote/Evernote/EvernoteTray.exe"&',
-	'xfdesktop' => 'xfdesktop &',
 );
 foreach my $key(keys %programmes){
 	system($programmes{$key}) if `pgrep $key|wc -l` == 0;
