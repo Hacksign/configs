d4ecc61a6d363145dd78ca13a8f3e85182dd60c0
Hacksign <evilsign@gmail.com>
2015-01-26 10:34:31 +0800

    add missing urls, and remove ads div in dashboard page

--- firefox/plugins/greasemonkey/inoreader.user.js
+++ firefox/plugins/greasemonkey/inoreader.user.js
@@ -1,11 +1,20 @@
 // ==UserScript==
 // @name        Remove inoreader ADS
 // @description Remove inoreader.com's advertisement in reading list, and upgrade button.
-// @version     1
+// @version     2
 // @grant       none
-// @include     https://www.inoreader.com/feed*
-// @include     https://www.inoreader.com/all_articles*
+// @include     https://www.inoreader.com/feed/*
 // @include     https://www.inoreader.com/folder/*
+// @include     https://www.inoreader.com/all_articles
+// @include     https://www.inoreader.com/starred
+// @include     https://www.inoreader.com/dashboard
+// @include	https://www.inoreader.com/trending
+// @include	https://www.inoreader.com/liked
+// @include	https://www.inoreader.com/commented
+// @include	https://www.inoreader.com/recent
+// @include	https://www.inoreader.com/web_pages
+// @include	https://www.inoreader.com/channel
+// @include	https://www.inoreader.com/my_channel
 // @namespace   https://raw.githubusercontent.com/Hacksign/configs/master/firefox/plugins/greasemonkey/inoreader.user.js
 // @downloadURL https://raw.githubusercontent.com/Hacksign/configs/master/firefox/plugins/greasemonkey/inoreader.user.js
 // @updateURL   https://raw.githubusercontent.com/Hacksign/configs/master/firefox/plugins/greasemonkey/inoreader.user.js
@@ -31,6 +40,16 @@ if (reader_pane_div) {
   reader_pane_div.addEventListener('DOMNodeInserted', function (e) {
     if (e.originalTarget.id && e.originalTarget.id.indexOf('leaderboard_ad-') != - 1) {
       e.originalTarget.parentNode.removeChild(e.originalTarget);
+    } else if (e.originalTarget.classList && e.originalTarget.classList.contains('dashboard_gadgets')) {
+      //find and remove ads div in dashboard page
+      sub_divs = e.originalTarget.getElementsByTagName('div');
+      for (i = 0; i < sub_divs.length; ++i) {
+        if (sub_divs[i].classList && sub_divs[i].classList.contains('header_control')) {
+          if (!sub_divs[i].innerHTML.trim()) {
+            e.originalTarget.parentNode.removeChild(e.originalTarget);
+          }
+        }
+      }
     }
   }, false);
 }
