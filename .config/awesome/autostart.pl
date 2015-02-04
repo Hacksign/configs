aa40a23a2e9be9189cf5be2b45e6b0a0e79b2a5c
Hacksign <evilsign@gmail.com>
2015-02-04 11:45:46 +0800

    update

--- .config/awesome/autostart.pl
+++ .config/awesome/autostart.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/perl
 
 my %programmes = (
+	'xfdesktop' => 'xfdesktop &',
 	'compton'	=>	'compton -bf -o 0.85 -m 0.85 -D9 -O 0.05 -I 0.05 &',
 	'volumeicon' => 'volumeicon &',
 	'thunar' => 'thunar --daemon &',
-	'wicd-client' => 'wicd-client -t&',
+	'nm-applet' => 'nm-applet&',
 	'fcitx' => 'fcitx -d &',
 	'launchy' => 'launchy &',
 	'goldendict' => 'goldendict &',
 	'thunderbird' => 'thunderbird &',
 	'myentunnel.exe' => 'wine "/home/hacksign/.wine/drive_c/Program Files/MyEnTunnel/myentunnel.exe" &',
 	'EvernoteTray' => 'wine  "/home/hacksign/.wine/drive_c/Program Files/Evernote/Evernote/EvernoteTray.exe"&',
-	'xfdesktop' => 'xfdesktop &',
 );
 foreach my $key(keys %programmes){
 	system($programmes{$key}) if `pgrep $key|wc -l` == 0;
