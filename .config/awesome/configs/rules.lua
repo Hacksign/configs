77800ccace063508fc2e2554b121cdd4bd2e7e25
Hacksign <evilsign@gmail.com>
2015-11-29 15:27:22 +0800

    update

--- .config/awesome/configs/rules.lua
+++ .config/awesome/configs/rules.lua
@@ -4,7 +4,10 @@ local beautiful = require("beautiful")
 
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-    awful.button({ modkey }, 1, awful.mouse.client.move),
+    awful.button({ modkey }, 1, function(c)
+        awful.mouse.client.move(c)
+        c:raise()
+    end),
     awful.button({ modkey }, 3, awful.mouse.client.resize)
 )
 awful.rules.rules = {
