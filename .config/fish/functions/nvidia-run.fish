bef8c3c73176c7f0c509162170d8abe2728a4444
Hacksign <evilsign@gmail.com>
2019-12-24 12:29:01 +0800

    fix multi graphic card problem

---
+++ .config/fish/functions/nvidia-run.fish
@@ -0,0 +1,3 @@
+function nvidia-run --description "Run a program with NVIDIA graphic card"
+    env __NV_PRIME_RENDER_OFFLOAD=1 __VK_LAYER_NV_optimus=NVIDIA_only __GLX_VENDOR_LIBRARY_NAME=nvidia "$argv"
+end
